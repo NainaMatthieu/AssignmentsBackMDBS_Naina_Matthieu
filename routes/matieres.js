cefab6393518a6b9c17de68c932e728e3e3dd5f9
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2024-05-27 21:49:53 +0300

    Modification fonction création matière

--- routes/matieres.js
+++ routes/matieres.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 let Matiere = require('../model/Matiere');
-
+let EtudiantMatiere = require('../model/etudiant_matiere');
 
 function getMatiereById(req, res) {
     let matiereId = req.params.id;
@@ -13,16 +13,25 @@ function getMatiereById(req, res) {
 // Ajout d'une matière (POST)
 function createMatiere(req, res){
     let matiere = new Matiere();
-    matiere.Matiere = req.body.Matiere;
-    matiere.image = req.body.image;
+    matiere.Matiere = req.body.nom;
+    matiere.mage = req.body.matiere_img;
     matiere.professeur_id = req.body.professeur_id;
     matiere.prof_img = req.body.prof_img;
+
+    let toAssign = req.body.toAssign
 
     matiere.save( (err) => {
         if(err){
             res.send('Erreur : ', err);
         }
-        res.json({ message: `${matiere.Matiere} enregistré !`})
+        res.json({ message: `${matiere} enregistré !`})
+        //Attribution de la matière à chaque élève
+        for (let etudiantId of toAssign) {
+            EtudiantMatiere.create({
+                idEtudiant: etudiantId,
+                idMatiere: matiere._id
+            });
+        }
     })
 }
 
