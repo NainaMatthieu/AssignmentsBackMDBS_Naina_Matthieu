ccefc4d7285d78247aa4159c1436dbfcc5157296
NainaMatthieu <andrianmattax@gmail.com>
2024-05-29 13:55:18 +0300

    route get All assignments

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.route(prefix + '/matieres/:id/prof')
 app.route(prefix + '/assignments')
   .post(assignment.postAssignment)
   .put(assignment.updateAssignment)
-//.get(assignment.getAssignments);
+  .get(assignment.getAssignments);
 
 app.route(prefix + '/user/rendre')
   .post(utilisateur.rendreAssignmentByEtudiant)
